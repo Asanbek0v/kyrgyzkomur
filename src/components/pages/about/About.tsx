44c1209b89e320dfab0e40ed5e846c807ee3e642
Дастан <dastan@example.com>
2026-08-12 15:27:21 +0600

    end HomePage Dastan

---
+++ src/components/pages/about/About.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import "./About.scss";
+
+const About: FC = () => {
+  return (
+    <section id="About">
+      <div className="container">
+        <div className="About">About</div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/pages/services/Services.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import "./Services.scss";
+
+const Services: FC = () => {
+  return (
+    <section id="Services">
+      <div className="container">
+        <div className="Services">services</div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
