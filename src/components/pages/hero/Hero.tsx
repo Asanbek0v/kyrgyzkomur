4290d75e4e0c8f8641fdb05a15e8059e8a1ae1c9
Дастан <dastan@example.com>
2026-09-08 15:52:16 +0600

    Fix styling and hero components

--- src/components/pages/hero/Hero.tsx
+++ src/components/pages/hero/Hero.tsx
@@ -83,7 +83,7 @@ const Hero = () => {
 
           <div className="btn-group" data-aos="zoom-in" data-aos-delay="400">
             <button className="primary-btn">
-              <span>Выбрать базу</span>
+              <span>Выбрать базуЗаказать доставку</span>
               <div className="btn-icon">➔</div>
             </button>
           </div>
